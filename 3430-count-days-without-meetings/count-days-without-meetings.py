499f1a4017b606a9c21e94638d020e92d3614086
Vignesh S <66085735+vignesh362@users.noreply.github.com>
2025-02-08 16:22:44 -0500

    Time: 243 ms (15.46%) | Memory: 53.1 MB (12.62%) - LeetSync

--- 3430-count-days-without-meetings/count-days-without-meetings.py
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -2,14 +2,13 @@ class Solution:
     def countDays(self, days: int, meetings: List[List[int]]) -> int:
 
         def isOverlap(p1,p2):
-            # print(p1,p2)
             return max(p1[0], p2[0]) <= min(p1[1], p2[1])
 
         def combinedTime(p1,p2):
             p1.extend(p2)
             return [min(p1),max(p1)]
+
         meetings=sorted(meetings, key= lambda x:x[0])
-        print(meetings)
         t=meetings[0]
         res=[]
         for i in range(1,len(meetings)):
@@ -18,7 +17,6 @@ class Solution:
             else:
                 res.append(t)
                 t=meetings[i]
-        print(res)
         res.append(t)
         for i,j in res:
             days-=(j-i+1)
