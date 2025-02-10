335639ff3cafdf0e796f5213a95a4b9804003b4c
Vignesh S <66085735+vignesh362@users.noreply.github.com>
2025-02-10 18:38:03 -0500

    Time: 958 ms (5.02%) | Memory: 44.2 MB (16.28%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,24 @@
+import heapq
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        hqNum=[]
+        for i,j in enumerate(nums):
+            hqNum.append((j,i))
+        heapq.heapify(hqNum)
+        print(hqNum)
+        marked=set()
+        res=0
+        while len(marked)!=len(nums):
+            j,i=heapq.heappop(hqNum)
+            if i not in marked:
+                marked.add(i)
+                res+=j
+                print(i,j)
+                if i-1>=0:
+                    marked.add(i-1)
+                if i+1<len(nums):
+                    marked.add(i+1)
+        return res
+
+
+        
