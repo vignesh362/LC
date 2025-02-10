ccf2a0d20af229ec134335e7382f638421785be3
Vignesh S <66085735+vignesh362@users.noreply.github.com>
2025-02-10 18:38:17 -0500

    Time: 563 ms (35.93%) | Memory: 44.3 MB (15.71%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.py
@@ -5,7 +5,7 @@ class Solution:
         for i,j in enumerate(nums):
             hqNum.append((j,i))
         heapq.heapify(hqNum)
-        print(hqNum)
+        # print(hqNum)
         marked=set()
         res=0
         while len(marked)!=len(nums):
@@ -13,7 +13,7 @@ class Solution:
             if i not in marked:
                 marked.add(i)
                 res+=j
-                print(i,j)
+                # print(i,j)
                 if i-1>=0:
                     marked.add(i-1)
                 if i+1<len(nums):
